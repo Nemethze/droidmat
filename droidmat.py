8949146c0a0c9fb35ca14c6823b58c15c79a615c
Nemethze <74901919+Nemethze@users.noreply.github.com>
2025-06-24 10:51:17 +0200

    Update droidmat.py

--- droidmat.py
+++ droidmat.py
@@ -8,14 +8,14 @@ def airplaneMode(time):
     os.system("sudo settings put global airplane_mode_on 1")
     os.system("sudo am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true")
     sleep(time)
-    os.system("sudo settings put global airplane_mode_off 1")
-    os.system("sudo am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false")
+    os.system("settings put global airplane_mode_on 0")
+    os.system("am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false")
 
 # rákeres az adott keywordre
 def search(keyword):
    search_url = f"https://www.google.com/search?q={keyword}"
    os.system(f'am start -n com.android.chrome/com.google.android.apps.chrome.Main -a android.intent.action.VIEW -d "{search_url}" --ez create_new_tab true --ez incognito true')
 
-
+# próbacseresznye
 airplaneMode(3)
-search(lufi)
+search("lufi")
