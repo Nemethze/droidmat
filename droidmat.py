1092087dcbf189b2bcf0304151bd9532585ddf0c
Nemethze <74901919+Nemethze@users.noreply.github.com>
2025-06-24 10:55:55 +0200

    Update droidmat.py

--- droidmat.py
+++ droidmat.py
@@ -2,14 +2,11 @@ import os
 from time import sleep
 import uiautomator as u
 
-
 # airplane mode be-ki kapcsolás !!!TSU installálása és ROOT legyen!!!
 def airplaneMode(time):
     os.system("sudo settings put global airplane_mode_on 1")
-    os.system("sudo am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true")
     sleep(time)
     os.system("sudo settings put global airplane_mode_on 0")
-    os.system("sudo am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false")
 
 # rákeres az adott keywordre
 def search(keyword):
